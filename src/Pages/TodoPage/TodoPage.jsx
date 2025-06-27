69fd95c64dee9efb3f99247a249605ceabe86df3
liza <vlizalizav@gmail.com>
2025-06-27 21:25:53 +0300

    commit Liza

--- src/Pages/TodoPage/TodoPage.jsx
+++ src/Pages/TodoPage/TodoPage.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function TodoPage() {
     return (
-        <h1>Todo page</h1>
+       <>
+    <h1>BookMood</h1>
+    <h2>Книги під настрій</h2>
+        <label for="mood" class="mood">Ваш настрій:</label>
+        <select id="mood">
+        <option value="relax" class="relax">Хочу розслабитися</option>
+        <option value="sad" class="sad">Сумно</option>
+        <option value="adventure" class="adventure">Хочу пригод</option>
+        <option value="love" class="love">Хочу любовну історію</option>
+      </select>
+
+</>
     )
 }
