1a1c422dd3f20ba475b19bea5ed5a8df1a93b081
Stas <stas11ipatov@gmail.com>
2025-06-28 12:29:39 +0300

    button

--- src/Pages/WeatherPage/WeatherPage.jsx
+++ src/Pages/WeatherPage/WeatherPage.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './Weather.css'
 
 export default function WeatherPage() {
+    const [display, setDisplay] = useState("none")
+    
     return (
+        <>
         <h1>Weather page</h1>
+        {/* <button onClick={function(){
+            if(display == 'none'){
+                setDisplay('block')
+            } else {
+                setDisplay('none')
+            }
+        }}>open</button> */}
+        <button onClick={()=>setDisplay(display == "none"? "block":"none")}>open2</button>
+        <div className='hidenMenu' style={{display:display}}>
+            <p>1</p>
+            <p>1</p>
+            <p>1</p>
+        </div>
+        </>
+
     )
 }
